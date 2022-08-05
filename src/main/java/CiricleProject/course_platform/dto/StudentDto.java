5716edf29ed16d70a4e8bdbb2d7740f883d26891
isomiddin <isomiddindehqonov2001@gmail.com>
2022-08-05 15:19:13 +0500

    add MenterAll

--- src/main/java/CiricleProject/course_platform/dto/StudentDto.java
+++ src/main/java/CiricleProject/course_platform/dto/StudentDto.java
@@ -1,4 +1,14 @@
 package CiricleProject.course_platform.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentDto {
+
 }
---
+++ src/main/java/CiricleProject/course_platform/service/impl/MenterImpl.java
@@ -0,0 +1,4 @@
+package CiricleProject.course_platform.service.impl;
+
+public class MenterImpl {
+}
