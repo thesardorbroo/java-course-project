6914a253bfb58c09ec32927fe9cb2196d0db0299
Sardor Shorahimov <thesardorisfire@gmail.com>
2022-08-05 14:22:16 +0500

    Repository is created!

---
+++ src/main/java/CiricleProject/course_platform/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package CiricleProject.course_platform.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository {
+}
