627430198fd29e453b6bd0dc51d681835f0e1a48
Sardor Shorahimov <thesardorisfire@gmail.com>
2022-08-05 12:14:37 +0500

    Project is created!

---
+++ src/main/java/CiricleProject/course_platform/CoursePlatformApplication.java
@@ -0,0 +1,13 @@
+package CiricleProject.course_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePlatformApplication.class, args);
+	}
+
+}
